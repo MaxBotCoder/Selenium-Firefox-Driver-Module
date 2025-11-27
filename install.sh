9ac33db0a17e63b36a58be4af1175268b83dc73e
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-11-27 08:15:02 -0800

    Delete install.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-python3 ~/Downloads/Selenium-Firefox-Driver-Module-main/message.py
-
-mv ~/Downloads/Selenium-Firefox-Driver-Module-main/firefox-webdriver-module-1.0 ~/.local/lib/python3.10/site-packages
