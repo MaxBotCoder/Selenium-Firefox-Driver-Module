9951a37e4dc3cbf36091d3ebffcd02ce1a979eaa
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-11-27 08:15:29 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+Welcome to Selenium Firefox Driver Modules's github page!
+
+The goal of this project is to create an easy and simple solution to enabling selenium on firefox!
+
+Version 1.0: Mac, Windows, Linux are now fully supported! 
---
+++ install-windows.cmd
@@ -0,0 +1,3 @@
+@python3 "%USERPROFILE%\Downloads\Selenium-Firefox-Driver-Module-main\message.py"
+
+move "%USERPROFILE%\Downloads\firefox-webdriver-module-1.0\firefox-webdriver-module-1.0" "%USERPROFILE%\AppData\Local\Programs\Python\Python310\Lib\site-packages"
