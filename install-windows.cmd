77b9f0f745aa480231ed59454ef0d70c27cc8700
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-11-27 19:54:13 -0800

    Update install-windows.cmd

--- install-windows.cmd
+++ install-windows.cmd
@@ -1,3 +1,3 @@
 @python3 "%USERPROFILE%\Downloads\Selenium-Firefox-Driver-Module-main\message.py"
 
-move "%USERPROFILE%\Downloads\firefox-webdriver-module-1.2\firefox-webdriver-module-1.2" "%USERPROFILE%\AppData\Local\Programs\Python\Python310\Lib\site-packages"
+move "%USERPROFILE%\Downloads\firefox-webdriver-module-1.2\firefox-webdriver-module.py" "%USERPROFILE%\AppData\Local\Programs\Python\Python310\Lib\site-packages"
