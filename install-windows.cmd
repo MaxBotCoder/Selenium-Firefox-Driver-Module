5d64214923e1fd0b000bafc65c4b173513aff947
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-11-27 22:06:55 -0800

    Add files via upload

---
+++ install-windows.cmd
@@ -0,0 +1,3 @@
+@python3 "%USERPROFILE%\Downloads\Selenium-Firefox-Driver-Module-main\message.py"
+
+move "%USERPROFILE%\Downloads\Selenium-Firefox-Driver-Module-main\fx_driver_module" "%USERPROFILE%\AppData\Local\Programs\Python\Python310\Lib\site-packages"
