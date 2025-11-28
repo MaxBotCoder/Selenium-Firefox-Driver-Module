4484400b0e116b6140d6b1eb874bed6b483b6210
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-11-27 21:25:17 -0800

    Delete install-unix.sh

--- install-unix.sh
+++
@@ -1,3 +0,0 @@
-python3 /home/max/Downloads/Selenium-Firefox-Driver-Module-main/message.py
-
-mv ~/Downloads/Selenium-Firefox-Driver-Module-main/firefox-webdriver-module-1.2 ~/.local/lib/python3.10/site-packages
