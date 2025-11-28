38888915bb41c0d279bb60cffd9af745dfcce263
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-11-27 21:25:23 -0800

    Delete install-windows.cmd

--- install-windows.cmd
+++
@@ -1,3 +0,0 @@
-@python3 "%USERPROFILE%\Downloads\Selenium-Firefox-Driver-Module-main\message.py"
-
-move "%USERPROFILE%\Downloads\Selenium-Firefox-Driver-Module-main\firefox-webdriver-module-1.2" "%USERPROFILE%\AppData\Local\Programs\Python\Python310\Lib\site-packages"
