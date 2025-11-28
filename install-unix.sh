02a6f3f4952ee1e1deca58c462f8bf7c74ea457c
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-11-27 19:56:01 -0800

    Update install-unix.sh

--- install-unix.sh
+++ install-unix.sh
@@ -1,3 +1,3 @@
 python3 /home/max/Downloads/Selenium-Firefox-Driver-Module-main/message.py
 
-mv ~/Downloads/Selenium-Firefox-Driver-Module-main/firefox-webdriver-module-1.0 ~/.local/lib/python3.10/site-packages
+mv ~/Downloads/Selenium-Firefox-Driver-Module-main/firefox-webdriver-module-1.2 ~/.local/lib/python3.10/site-packages
